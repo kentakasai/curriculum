1b4bf1bd86aede38333d3eb14c4d3f217efe1c14
kentakasai <79413880+kentakasai@users.noreply.github.com>
2021-03-31 03:40:16 +0900

    Task1-1.java作成
    課題３までやった状態
    上書きはしてない

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,11 @@
+public class Task1-1{
+    public static void main(String[]args){
+        int hogeInt=50;
+        String hogeString="テスト";
+        //double型 hogeDoubleという変数名　値0.2で初期化
+        double hogeDouble=0.2;
+
+        
+
+    }
+}
