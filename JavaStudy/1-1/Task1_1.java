1b5aa5e34a2d3a1a893910d3c204c9a23a4146e8
kentakasai <79413880+kentakasai@users.noreply.github.com>
2021-03-31 03:48:51 +0900

    課題４−８
    課題４−８まで完了

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -5,7 +5,13 @@ public class Task1-1{
         //double型 hogeDoubleという変数名　値0.2で初期化
         double hogeDouble=0.2;
 
-        
+        hogeInt=100;
+        hogeString="合格";
+        System.out.println(hogeInt);
+        //hogeStringの値を表示
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+
 
     }
 }
